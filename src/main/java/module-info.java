dc41dd8b3b8ad2beaf613cbc086df218409080ad
Xiang115 <gohkimxiang0115goh@gmail.com>
2025-03-01 16:19:35 +0800

    :seedling: init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.hospital_ppe_inventory_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.hospital_ppe_inventory_system to javafx.fxml;
+    exports org.example.hospital_ppe_inventory_system;
+}
